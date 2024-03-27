9aae168ef04b1b4de51620a001ce1a7b6af7300e
Pablo Periañez <pabpercab1@alum.us.es>
2024-03-27 13:55:07 +0100

    Initial Commit

---
+++ ExamUS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ExamUS
+//
+//  Created by Pablo Periañez Cabrero on 27/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ExamUS/ExamUSApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExamUSApp.swift
+//  ExamUS
+//
+//  Created by Pablo Periañez Cabrero on 27/3/24.
+//
+
+import SwiftUI
+
+@main
+struct ExamUSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
